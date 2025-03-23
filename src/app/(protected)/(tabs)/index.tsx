395554c572c2bed37d69b86f3e83d0ef1d72ff31
ahmet-24810Fun <dragonsfire20201998@gmail.com>
2025-03-23 21:50:41 +0300

    supabase design

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,9 +1,43 @@
-import { View, FlatList } from "react-native";
+import {useState, useEffect} from "react";
+import {Text, View, FlatList, ActivityIndicator } from "react-native";
 import PostListItem from "../../../components/PostListItem";
-import posts from "../../../../assets/data/posts.json";
+//import posts from "../../../../assets/data/posts.json";
+
+import {supabase} from "../../../lib/supabase";
+//import { Post } from "../../../types";
+import { Tables } from "../../../types/database.types";
+import { useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../../../services/postService";
+
+
+type Post = Tables<"posts"> & {
+  group: Tables<"groups">;
+  user: Tables<"users">;
+}
+
+
+
 
 export default function HomeScreen() {
+
+  const { data: posts, isLoading, error} = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => fetchPosts()
+  })
+  
  
+
+
+
+ if (isLoading) {
+  return <ActivityIndicator />;
+}
+
+if (error) {
+  console.log(error);
+  return <Text>Error fetching posts</Text>;
+}
+
   return (
     <View>
     <FlatList 
--- src/app/(protected)/post/[id].tsx
+++ src/app/(protected)/post/[id].tsx
@@ -1,25 +1,37 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import posts from "../../../../assets/data/posts.json";
 import PostListItem from "../../../components/PostListItem";
+import { fetchPostsById } from "../../../services/postService";
+import { useQuery } from "@tanstack/react-query";
 
 export default function DetailedtPost() {
 
-  const { id } = useLocalSearchParams();
+  const { id } = useLocalSearchParams<{id : string}>();
+
+  const {data : post, isLoading, error} = useQuery({
+    queryKey: ["posts", id],
+    queryFn: () => fetchPostsById(id)
+  });
 
   const detailedPost = posts.find((post) => post.id === id);
   
-  if (!detailedPost) {
+  
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error || !post) {
     return (
       <View>
         <Text>Post not found</Text>
       </View>
     );
   }
-
   return (
     <View>
-        <PostListItem post={detailedPost} isDetailedPost={true}/>
+        <PostListItem post={post} isDetailedPost={true}/>
     </View>
   );
 }
